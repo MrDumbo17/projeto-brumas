4a1c4803f74b9f55a9823f1d1d65b081eaa2ef42
Mister Dumbo <110036209+MrDumbo17@users.noreply.github.com>
2022-08-08 09:10:12 -0300

    mais mudanças

---
+++ script.js
@@ -0,0 +1,10 @@
+function carregar(){
+
+    var inp_per = document.querySelectorAll('input.pericia')
+    var qtd_per = inp_per.length()
+
+        
+    var teste = document.querySelector('div#teste')
+
+    teste.style.color = 'green'
+}
