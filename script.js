fb5e2f3aca8ca3adf4b429a90d6c5c1f574e62f5
Mister Dumbo <110036209+MrDumbo17@users.noreply.github.com>
2022-08-11 17:50:18 -0300

    teste

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 function carregar(){
 
     var inp_per = document.querySelectorAll('input.pericia')
-    var qtd_per = inp_per.length()
 
-        
-    var teste = document.querySelector('div#teste')
-
-    teste.style.color = 'green'
+    for(let i  in inp_per) {
+        inp_per[i].value = 0
+    }
 }
