c584f9ad9520b660737bff425e99ba3213423bcb
Mister Dumbo <110036209+MrDumbo17@users.noreply.github.com>
2022-10-20 08:55:11 -0300

    01

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
+
+var allinp = document.querySelectorAll('input')
+
+var inp_per = document.querySelectorAll('input.pericia')
+
 function carregar(){
 
-    var inp_per = document.querySelectorAll('input.pericia')
 
-    for(let i  in inp_per) {
+    allinp.forEach((valor,i) => valor.addEventListener('change',function(){testval(i)}))
+
+    allinp.forEach((valor,i) => valor.addEventListener('focus',function(){limpa(i)}))
+
+
+    inp_per.forEach((set) => set.value = 0) 
+    
+    
+   /* for(let i  in inp_per) {
         inp_per[i].value = 0
+    } */
+}
+
+
+function testval(x = 0) {
+
+    if(!allinp[x].checkValidity()) {
+        
+        window.alert('Valor invalido')
+        allinp[x].focus()
     }
+}
+
+function limpa(x = 0) {
+    
+    allinp[x].value = ' '
 }
